7a83d9eaa81e6eeea2c5ba86793ab49d7debf17f
Aleksandr Sysoev <aosysoev@yandex.ru>
2022-08-16 23:56:20 +0300

    second task in progress

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Tag, ArticleTag
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ArticleTag)
+class ArticleTagAdmin(admin.ModelAdmin):
+    pass
+
+class ScopeInLine(admin.TabularInline):
+    model = ArticleTag
+    extra = 1
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInLine,]
+
--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=25, verbose_name='Раздел')
+
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+    
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
 
@@ -7,10 +17,18 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, through='ArticleTag', related_name='articles')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at',]
 
     def __str__(self):
         return self.title
+
+class ArticleTag(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Статья')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Тема')
+    is_main = models.BooleanField(default='False', verbose_name='Основной')
+    
